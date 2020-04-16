93c5a7ca9e94e34987ff99c3e29d1f83c358a886
Osha G <ogroetz@gmail.com>
2020-04-16 03:11:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+array = ["kiwi"]
 def oxford_comma(array)
-
+  if array.length == 1 
+  array.join
+  elsif array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length > 2 
+  array[-2] << " and "
+  array.join(", ")
+  end
 end
+
